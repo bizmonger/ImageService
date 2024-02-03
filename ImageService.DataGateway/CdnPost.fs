511d5f2865cc83d10174e0c9d6fc4f2a67ce2668
Scott Nimrod <scott.nimrod@bizmonger.net>
2024-02-03 08:51:06 -0500

    Added files for CDN support

---
+++ ImageService.DataGateway/CdnPost.fs
@@ -0,0 +1,27 @@
+﻿namespace ImageService.DataGateway.Cdn
+
+open BeachMobile.ImageService.Operations
+
+module Upload =
+
+    let image : Upload.Image = 
+    
+        fun image -> task { return Error "TODO" }
+
+    let images : Upload.Images = 
+    
+        fun image -> task { return Error "TODO" }
+
+module Containers =
+
+    let add : Container.Add =
+
+        fun v -> task { return Error "TODO" }
+
+    let remove : Container.Remove =
+
+        fun v -> task { return Error "TODO" }
+
+    let removeImages : Container.RemoveImages =
+
+        fun v -> task { return Error "TODO" }
---
+++ ImageService.DataGateway/CdnQuery.fs
@@ -0,0 +1,17 @@
+﻿namespace ImageService.DataGateway.Cdn
+
+open BeachMobile.ImageService.Operations
+
+module Download =
+
+    let Item : Download.Item =
+
+        fun v -> task { return Error "TODO" }
+
+    let container : Download.Container =
+
+        fun v -> task { return Error "TODO" }
+
+    let all : Download.All =
+
+        fun v -> task { return Error "TODO" }

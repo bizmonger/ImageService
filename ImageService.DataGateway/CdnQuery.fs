542b91b96eb7b199b554ad33c80532d80855f70a
Scott Nimrod <scott.nimrod@bizmonger.net>
2024-02-05 08:02:03 -0500

    Misc.

--- ImageService.DataGateway/CdnQuery.fs
+++ ImageService.DataGateway/CdnQuery.fs
@@ -2,6 +2,8 @@
 
 open BeachMobile.ImageService.Operations
 
+// https://learn.microsoft.com/en-us/azure/cdn/cdn-create-a-storage-account-with-cdn?toc=%2Fazure%2Ffrontdoor%2FTOC.json
+
 module Download =
 
     let Item : Download.Item =
